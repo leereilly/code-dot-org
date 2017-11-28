cfe2509dfd01e5596343c9e0657bd3303c71e8cd
asherkach <asher.kach@gmail.com>
2017-11-28 16:08:26 -0600

    Fixes violations of rubocop Security/JSONLoad.

--- shared/middleware/files_api.rb
+++ shared/middleware/files_api.rb
@@ -487,7 +487,7 @@ class FilesApi < Sinatra::Base
       #     }
       #   ]
       # }
-      {"filesVersionId": result[:version_id], "files": JSON.load(result[:body])}.to_json
+      {"filesVersionId": result[:version_id], "files": JSON.parse(result[:body])}.to_json
     end
   end
 
@@ -651,7 +651,7 @@ class FilesApi < Sinatra::Base
     bucket = FileBucket.new
     manifest_result = bucket.get(encrypted_channel_id, FileBucket::MANIFEST_FILENAME)
     not_found if manifest_result[:status] == 'NOT_FOUND'
-    manifest = JSON.load manifest_result[:body]
+    manifest = JSON.parse manifest_result[:body]
 
     # remove the file from the manifest
     manifest_delete_comparison_filename = CGI.unescape(filename).downcase
@@ -694,7 +694,7 @@ class FilesApi < Sinatra::Base
     bucket = FileBucket.new
     manifest_result = bucket.get(encrypted_channel_id, FileBucket::MANIFEST_FILENAME, nil, params['version'])
     bad_request if manifest_result[:status] == 'NOT_FOUND'
-    manifest = JSON.load manifest_result[:body]
+    manifest = JSON.parse manifest_result[:body]
 
     # restore the files based on the versions stored in the manifest
     manifest.each do |entry|
