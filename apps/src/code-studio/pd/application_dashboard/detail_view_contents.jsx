28a35470277a14f927109432133e781a711d2aaf
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-11-30 16:43:20 -0800

    include locked property

--- apps/src/code-studio/pd/application_dashboard/detail_view_contents.jsx
+++ apps/src/code-studio/pd/application_dashboard/detail_view_contents.jsx
@@ -31,6 +31,7 @@ export default class DetailViewContents extends React.Component {
     applicationId: PropTypes.string.isRequired,
     applicationData: PropTypes.shape({
       regional_partner_name: PropTypes.string,
+      locked: PropTypes.bool,
       notes: PropTypes.string,
       status: PropTypes.string.isRequired,
       school_name: PropTypes.string,
@@ -50,6 +51,7 @@ export default class DetailViewContents extends React.Component {
 
   state = {
     status: this.props.applicationData.status,
+    locked: this.props.applicationData.locked,
     notes: this.props.applicationData.notes || "Google doc rubric completed: Y/N\nTotal points:\n(If interviewing) Interview notes completed: Y/N\nAdditional notes:",
     response_scores: this.props.applicationData.response_scores || {},
     editing: false
@@ -59,6 +61,7 @@ export default class DetailViewContents extends React.Component {
     this.setState({
       editing: false,
       status: this.props.applicationData.status,
+      locked: this.props.applicationData.locked,
       notes: this.props.applicationData.notes
     });
   };
