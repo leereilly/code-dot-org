d2f63594dce9fe4fd6a20a5679996cd6b76e347e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-11-30 16:40:19 -0800

    use js-friendly keys for locked status

--- dashboard/app/serializers/api/v1/pd/application_serializer.rb
+++ dashboard/app/serializers/api/v1/pd/application_serializer.rb
@@ -1,9 +1,13 @@
 class Api::V1::Pd::ApplicationSerializer < ActiveModel::Serializer
-  attributes :regional_partner_name, :locked?, :notes, :form_data, :status,
+  attributes :regional_partner_name, :locked, :notes, :form_data, :status,
     :school_name, :district_name, :email, :application_type, :response_scores
 
   def email
     object.user.email
+  end
+
+  def locked
+    object.locked?
   end
 
   # Include the full answers here
